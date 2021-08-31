6d7f64c814f2283f4ba06314cae77833b897e4e6
alexlewis1990 <alexlewis199015@gmail.com>
2021-08-31 18:21:36 -0400

    first commit

---
+++ GameFinder.Models/GF_Models.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GameFinder.Models
+{
+    class GF_Models
+    {
+        public string GameID { get; set; }
+        public decimal PriceRange { get; set; }
+        public string Rating { get; set; }
+        public string system { get; set; }
+        public string GameTitle { get; set; }
+    }
+}
